087a9bc23a1202b63af1b49ee0628378ee94714d
horang <fnelclsrn123@gmail,com>
2024-10-11 16:16:38 +0900

    refactor: 변경

--- src/main/java/com/jafar/api/config/SwaggerConfig.java
+++ src/main/java/com/jafar/api/config/SwaggerConfig.java
@@ -44,5 +44,6 @@ public class SwaggerConfig {
                         .addServersItem(new Server().url(serverUrl))
                         .addSecurityItem(securityRequirement)
                         .components(components);
+
         }
 }
