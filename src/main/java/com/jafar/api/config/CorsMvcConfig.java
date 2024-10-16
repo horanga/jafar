2244ab6e7eec97ef48297f54490a68ef558dfcd7
kimkyoungyeon <marycarychin@gmail.com>
2024-10-17 05:38:48 +0900

    Update CorsMvcConfig.java

--- src/main/java/com/jafar/api/config/CorsMvcConfig.java
+++ src/main/java/com/jafar/api/config/CorsMvcConfig.java
@@ -12,6 +12,6 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
                 .exposedHeaders("Set-Cookie")
-                .allowedOrigins("http://43.203.233.134:3000","http://43.203.233.134:3002");
+                .allowedOrigins("http://43.203.233.134:3000","http://43.203.233.134:3002","http://localhost:3000", "http://localhost:3002");
     }
 }
